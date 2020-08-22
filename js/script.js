82b33b3e806126665d06ea6f58d7fd6e8f030639
cnnrbrn <connorbobrien@gmail.com>
2020-08-22 11:14:53 +0200

    Add first files

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const url = "https://cat-fact.herokuapp.com/factss";
+
+const resultsContainer = document.querySelector(".results");
+
+async function getCats() {
+    try {
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        const facts = data.all;
+
+        resultsContainer.innerHTML = "";
+
+        for (let i = 0; i < facts.length; i++) {
+            console.log(facts[i].text);
+
+            if (i === 10) {
+                break;
+            }
+
+            resultsContainer.innerHTML += `<div class="result">${facts[i].text}</div>`;
+        }
+    } catch (error) {
+        console.log("An error occurred");
+        resultsContainer.innerHTML = displayError("An error occurred when calling the API");
+    }
+}
+
+getCats();
